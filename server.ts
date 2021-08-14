3fe0270283ce2fedf791f4d1591e985df9f4c533
lerichardson <litbelb1@outlook.com>
2021-08-14 15:46:56 -0700

    Change auth to auth0

--- server.ts
+++ server.ts
@@ -1,21 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const Identity = require('./src/identity.js');
+const { auth } = require('express-openid-connect');
 import * as path from "path";
 const app = express();
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_HASHING_KEY,
+  baseURL: 'http://localhost:3000',
+  clientID: 'CQmqCPOL46mDy75l8LOdzminGaFIBYy2',
+  issuerBaseURL: 'https://parakeet-cloud.eu.auth0.com'
+};
 
 app.set('views', path.resolve('public'));
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(cors());
+app.use(auth(config));
 app.use(helmet());
 app.get('/', (req: any, res: any) => {
   res.render('index.pug')
-})
-// Identity
-app.get('/identity', (req: any, res: any) => {
-  Identity.loginOrSignup(req.query.email)
 })
   app.listen(3000, () => {
     console.log(`App listening at http://localhost:3000`)
